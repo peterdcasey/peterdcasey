5b0e866caa0f1455fb3430c9aff3fea87651392c
Peter Casey <pcasey@cocc.edu>
2017-10-19 16:00:33 -0700

    whatever sketch

---
+++ p5_processing/whatever/sketch.js
@@ -0,0 +1,27 @@
+var cnv;
+
+function centerCanvas() {
+  var x = (windowWidth - width) / 2;
+  var y = (windowHeight - height) / 2;
+  cnv.position(x, y);
+}
+
+function setup() {
+  cnv = createCanvas(600, 600);
+  centerCanvas();
+  background(255, 200, 200);
+}
+
+function windowResized() {
+  centerCanvas();
+}
+
+function draw() {
+  fill(30,40,200, 100);
+
+  for (let x = 10; x <= 200; x += 10) {
+    fill(x + 30, x * 5, x * 10, 100);
+    rect(x, x + 5, x * 5, 200);
+  }
+
+}
